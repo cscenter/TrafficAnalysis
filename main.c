3d2b61e58fe874368ae5c86b09fac4736e6d348b
Andrey <andrsuh@rambler.ru>
2015-03-28 20:20:05 +0300

    main

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
+//#include <stdlib.h>
+//#include <ctype.h>
+//#include <errno.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//#include <map>
+
 #include <pcap.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <iostream>
 #include <new>
-
-#include <ctype.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <map>
-
 #include "class_sniff.h"
 #include "StatisticAnalysis.h"
 #include "SignatureAnalisator.c"
+
 
 #define SNAP_LEN 1518
 #define SIZE_ETHERNET 14
@@ -24,11 +24,12 @@
 
 
 
+
 int main(int argc, char **argv) {
 	char protocol[] = "ip";
     allPackets p;
 	if (argc == 2) {
-			NetSniffer *obj = new NetSniffer(argv[1], protocol, 10);
+			NetSniffer *obj = new NetSniffer(argv[1], protocol, 40);
 			p = obj->StartSniff();
 	}
 	else if (argc > 2) {
@@ -40,13 +41,12 @@ int main(int argc, char **argv) {
         p = obj->StartSniff();
 	}
 
-    cout << p.v.size() << endl;
     p.PrintVector();
 
     SignatureAnalisator *sig_analys = new SignatureAnalisator();
     sig_analys->FormMap(p.v);
     sig_analys->PrintMap();
-    //StatisticAnalysis * statAnalysis = new StatisticAnalysis(p);
+    StatisticAnalysis * statAnalysis = new StatisticAnalysis(p.v);
     printf("\nCapture complete.\n");
 
 	return 0;
