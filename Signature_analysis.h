1a49f98b9fd341946d251b0a947d0001070cfdb7
Andrey <andrsuh@rambler.ru>
2015-04-11 02:37:16 +0300

    +dinam proc +private field

--- Signature_analysis.h
+++ Signature_analysis.h
@@ -1,34 +1,43 @@
 #ifndef SIGNATURE_ANALISATOR_H
 #define SIGNATURE_ANALISATOR_H
+
 #include <map>
-//#include "Net_sniffer.h"
-#include "Session.h"
 #include <vector>
 #include <pcap.h>
 #include <netinet/in.h>
 #include "Pack_headers_struct.h"
 #include "Parse_packet.h"
+#include "Session.h"
 
-//EL add private
+
 class Pack_data {
-public:
-    struct in_addr src;
+
+    in_addr src;
+
     std::vector<u_char*> upload;
+
     std::vector<u_char*> download;
+
+public:
 
     Pack_data();
 
-    void form_pack_date(Session session, Split_packet pack);
+    void to_upload(Split_packet pack);
+
+    void to_download(Split_packet pack);
 
     int check_date(const char *expr);
 
     void print_payload(int length, const u_char *payload);
+
 };
 
+
 class Signature_analysis {
-private:
-    
+
     std::map<Session, Pack_data> Map;
+
+    Session get_session(Split_packet pack);
 
 public:
 
@@ -44,7 +53,6 @@ public:
 
     void add_packet(const Split_packet& pack);
 
-    Session get_session(Split_packet pack);
 };
 
 #endif
