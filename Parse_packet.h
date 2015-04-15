43a4db808ab193023694185dfb1b2865ac1779e1
Andrey <andrsuh@rambler.ru>
2015-04-15 09:30:05 +0300

    +Merge class Parse_packet and class Sprit_Packet in class Packet

--- Parse_packet.h
+++ Parse_packet.h
@@ -8,32 +8,52 @@
 
 #include "Pack_headers_struct.h"
 
-//EL хорошо бы совместить эти оба класса в один, чтобы поля хранились 
+#include "Session.h"
+
+//EL хорошо бы совместить эти оба класса в один, чтобы поля хранились
 //EL вместе с методами, которые с ними работают
 
 //EL нужны деструкторы! есть new, но нет delete => memory leak
 
-struct Split_packet {
-    struct pcap_pkthdr header;
-    struct sniff_ethernet ethernet;
-    struct sniff_ip ip;
-    struct sniff_tcp tcp;
-    struct sniff_udp udp;
+
+class Packet {
+    pcap_pkthdr header;
+
+    sniff_ethernet ethernet;
+
+    sniff_ip ip;
+
+    sniff_tcp tcp;
+
+    sniff_udp udp;
+
     u_char *payload;
+
     int size_ip;
+
     int size_tcp;
+
     int size_payload;
+
     int size_udp;
-    bool is_broken;
-};
 
-
-class Parse_packet {
 public:
 
-    Parse_packet();
+    Packet();
 
-    Split_packet Parse(const struct pcap_pkthdr *head, const u_char *packet);
+    bool is_broken;
+
+    void Parse(const pcap_pkthdr *head, const u_char *packet);
+
+    void get_session(Session& session)const;
+
+    inline int get_size_payload() {
+        return size_payload;
+    };
+
+    inline u_char* get_pload() {
+        return payload;
+    };
 };
 
 #endif // CLASS_PARSE_PACKET_H
