d32c8ea9040bd59358f0c48e1775e27b000856d0
Andrey <andrsuh@rambler.ru>
2015-04-16 22:32:54 +0300

    no struct in_addr

--- Session.h
+++ Session.h
@@ -7,19 +7,18 @@
 
 
 struct Session {
-    struct  in_addr ip_src;
-    struct  in_addr ip_dst;
+    in_addr ip_src;
+    in_addr ip_dst;
     u_short port_src;
     u_short port_dst;
     std::string prot;
     u_char protocol;
     int time_to_live;
-    int last_packet_time;    
+    int last_packet_time;
     Session(const Packet& p);
     Session();
     bool is_alive() const;
     void print_session();
-    //EL: move to cpp
     bool operator < (const Session & b) const;
     void session_reverse();
 };
