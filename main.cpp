d7fe1a5d1e98b8cac2934705dbca0047862286a8
Andrey <andrsuh@rambler.ru>
2015-04-18 00:48:56 +0300

    add argum of command line which allowed to choose the mode

--- main.cpp
+++ main.cpp
@@ -18,20 +18,28 @@ int main(int argc, char **argv) {
     char protocol[] = "ip";
     Working_classes wc;
     if (argc == 2) {
-            Net_sniffer *obj = new Net_sniffer(argv[1], protocol, 40);
-            wc = obj->start_sniff();
-    }
-    else if (argc > 2) {
-            cout << "error: unrecognized command-line options\n" << endl;
-            return 0;
+        Net_sniffer *obj = new Net_sniffer(argv[1], protocol, 0, "live");
+        wc = obj->start_sniff();
     }
     else {
-        Net_sniffer *obj = new Net_sniffer();
-        wc = obj->start_sniff();
+        if (argc == 3) {
+            Net_sniffer *obj = new Net_sniffer(argv[1], protocol, 0, argv[2]);
+            wc = obj->start_sniff();
+        }
+        else {
+            if (argc > 3) {
+                cout << "error: unrecognized command-line options\n" << endl;
+                return 0;
+            }
+            else {
+                Net_sniffer *obj = new Net_sniffer();
+                wc = obj->start_sniff();
+            }
+        }
     }
     //Statistic_analysis * stat_analysis = new StatisticAnalysis(p.v);
     //wc.stat_analysator.print_map();
-    wc.sig_analysator.print_sessions_list();
+    //wc.sig_analysator.print_sessions_list();
     cout << "Capture complete" << endl;
     return 0;
 };
