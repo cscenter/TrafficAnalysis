a0ffeacf48bbc2875bfc084247d2ed9ed0bdcc78
Andrey <andrsuh@rambler.ru>
2015-04-24 20:44:06 +0300

    + priority-field

--- Config.h
+++ Config.h
@@ -15,7 +15,7 @@ public:
 
     Config(const char* f_name);
 
-    bool get_next_signature(std::string& signature, std::string& type);
+    bool get_next_signature(std::string& signature, std::string& type, int *priority);
 
     bool is_ready() { return !in_process; }
 };
