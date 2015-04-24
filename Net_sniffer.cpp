e763e7ecc1fc2f4acf0f80246c236ffd471842f1
Andrey <andrsuh@rambler.ru>
2015-04-24 20:47:30 +0300

    - num_pack + wc_pointer_transmission + static arrays

--- Net_sniffer.cpp
+++ Net_sniffer.cpp
@@ -10,74 +10,58 @@
 using namespace std;
 
 Net_sniffer::Net_sniffer() {
-    dev = NULL;
-    //strcpy(filter_exp,"\0ip");// недоработанный
-    num_packets = 100;
+    strcpy(dev, "");
+    is_live = true;
+    strcpy(filter_exp, "ip");
 };
 
 
 
-Net_sniffer::Net_sniffer(char *device, char *protocol, int n, const char *m) {
-    //EL change to static arrays или как минимум надо память освобождать
+Net_sniffer::Net_sniffer(char *device, char *protocol, bool mode) {
     //классу нужен деструктор
-    dev = (char *) malloc((sizeof(device))); //размер не под указатель?
+    dev = (char *) malloc((sizeof(device)));
     strcpy(dev, device);
-    filter_exp = (char *) malloc((sizeof(protocol)));
     strcpy(filter_exp, protocol);
-    num_packets = n;
-    //EL: сделат char mode[256 (и dev, filter)]
-    mode = (char *) malloc((sizeof(m)));
-    strcpy(mode, m);
+    is_live = mode;
 };
 
 
-Working_classes Net_sniffer::start_sniff(){
+void Net_sniffer::start_sniff(Working_classes* p){
 
     if ( dev == NULL) {
         // find a capture device if not specified on command-line
-
         dev = pcap_lookupdev(errbuf);
         if (dev == NULL) {
             fprintf(stderr, "Couldn't find default device: %s\n",
                 errbuf);
-            //EL: Заменить на эксепшены
             exit(EXIT_FAILURE);
         }
     }
 
     // get network number and mask associated with capture device
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1 && is_live) {
         fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
         net = 0;
         mask = 0;
     }
 
     printf("Device: %s\n", dev);
-    printf("Number of packets: %d\n", num_packets);
     printf("Filter expression: %s\n\n\n", filter_exp);
 
-    // open capture device
-    //EL: вся работа с командной строкой только в main. здесь bool isOnline
-    if (strstr(mode, "offline") != NULL) {
-        handle = pcap_open_offline(dev, errbuf);
+    if (is_live) {
+        handle = pcap_open_live(dev, SNAP_LEN, 0, 1000, errbuf);
     }
     else {
-        handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+        handle = pcap_open_offline(dev, errbuf);
     }
-
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        //EL: exception
         exit(EXIT_FAILURE);
     }
-    //EL сделать два варианта запуска без переписывания программмы: из файла или из устройства
-
-    // make sure we're capturing on an Ethernet device [2]
     if (pcap_datalink(handle) != DLT_EN10MB) {
         fprintf(stderr, "%s is not an Ethernet\n", dev);
         exit(EXIT_FAILURE);
     }
-
     // compile the filter expression
     /*if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
         fprintf(stderr, "Couldn't parse filter %s: %s\n",
@@ -92,28 +76,18 @@ Working_classes Net_sniffer::start_sniff(){
         exit(EXIT_FAILURE);
     }*/
 
-
-    //EL не нужно лишних копирования при возврате этой переменной
-    //EL надо передавать указатель из main
-    Working_classes p;
-
-    pcap_loop(handle, 10000, got_packet, (u_char *)(&p));
-
+    pcap_loop(handle, 0, got_packet, (u_char *)(p));
     pcap_freecode(&fp);
     pcap_close(handle);
-    return p;
 };
 
 void Net_sniffer::got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     Working_classes * wc = (Working_classes *) args;
-    //EL классы должны быть названы существительными, например, PacketParser
-    //Split_packet value;
     Packet *value = new Packet();
-    //EL нет смысла делать лишние копирования
     value->Parse(header, packet);
     if (!value->is_broken) {
-        wc->sig_analysator.add_packet(*value);
-        //wc->stat_analysator.add_packet(value);
+        wc->get_signature_analysis()->add_packet(*value);
+        //осторожно, я менял wc wc->get_statistic_analysys()->add_packet(*value);
     }
 }
 
--- Net_sniffer.h
+++ Net_sniffer.h
@@ -10,21 +10,21 @@
 class Net_sniffer {
 private:
     char *dev;              // device name
-    char *mode;
-    char *filter_exp;        // filter expression
+    bool is_live;
+    char filter_exp[10];        // filter expression
     char errbuf[PCAP_ERRBUF_SIZE];        // error buffer
     pcap_t *handle;                // packet capture handle
-    struct bpf_program fp;        // compiled filter program (expression)
+    bpf_program fp;        // compiled filter program (expression)
     bpf_u_int32 mask;            // subnet mask
     bpf_u_int32 net;            // ip
-    int num_packets;            // number of packets to capture
 public:
     Net_sniffer();
-    Net_sniffer(char *device, char *protocol, int n, const char *m);
+    Net_sniffer(char *device, char *protocol, bool mode);
+    ~Net_sniffer() { delete dev; }
 
-    Working_classes start_sniff();
+    void start_sniff(Working_classes *p);
 
-    static void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+    static void got_packet(u_char *args, const pcap_pkthdr *header, const u_char *packet);
 };
 
 #endif
