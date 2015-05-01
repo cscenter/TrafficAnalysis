94a164ff150729d2ce99dcf76ed31c60ea6fbc7e
Andrey <andrsuh@rambler.ru>
2015-05-01 21:22:36 +0300

    settings for class SAnalysis

---
+++ Signature_configurations.h
@@ -0,0 +1,24 @@
+#ifndef Signature_configurations_H
+#define Signature_configurations_H
+
+#include <string>
+#include "tinyxml/tinyxml.h"
+#include "tinyxml/tinystr.h"
+
+class Signature_configurations {
+private:
+	//EL: string
+    char file_name[100];
+    bool in_process;
+    TiXmlDocument document;
+    TiXmlElement *current_element;
+public:
+	//EL: лучше сделать метод bool load(...)
+    Signature_configurations(const char* f_name);
+
+    bool get_next_signature(std::string& signature, std::string& type, int *priority, int* num_pack);
+
+    bool is_ready() { return !in_process; }
+};
+
+#endif
