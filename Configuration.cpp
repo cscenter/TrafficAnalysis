420bf9828bece6a2826cad3ffbb7276a7f04eefa
Andrey <andrsuh@rambler.ru>
2015-05-03 18:30:55 +0300

    new version singlt-class union all Config*

--- Configuration.cpp
+++ Configuration.cpp
@@ -7,20 +7,92 @@ using namespace std;
 
 Config* Config::config = 0;
 
-Config::Config(const char* f_name) {
+Config::Config(std::string f_name) : file_name(f_name) {}
+
+bool Config::load_xml_file() {
     in_process = true;
-    strcpy(file_name, f_name);
-    bool load_status = document.LoadFile(file_name, TIXML_DEFAULT_ENCODING);
+    bool load_status = document.LoadFile(file_name.c_str(), TIXML_DEFAULT_ENCODING);
     if (!load_status) {
-        cout << "Have a load mistake!" << endl;
-        exit(0);
+        return false;
     }
     current_element = document.RootElement()->FirstChildElement();
+    return true;
+}
+
+bool Config::get_next_signature(string& signature, string& type, int *priority, int *num_pack) {
+    if (!in_process) {
+        return false;
+    }
+    TiXmlAttribute *atr = current_element->FirstAttribute();
+    int atr_num = 1;
+    while (atr != NULL) {
+        switch(atr_num) {
+            case 1 : signature = *(new string(atr->Value()));
+                          break;
+            case 2 : type = *(new string(atr->Value()));
+                          break;
+            case 3 : int pr;
+                              if (atr->QueryIntValue(&pr) == TIXML_SUCCESS) {
+                                  *priority = pr;
+                              }
+                              else {
+                                  return false;
+                              }
+                              break;
+            case 4 : int num;
+                              if (atr->QueryIntValue(&num) == TIXML_SUCCESS) {
+                                  *num_pack = num;
+                              }
+                              else {
+                                  return false;
+                              }
+                              break;
+            default : return false;
+        }
+        atr = atr->Next();
+        atr_num++;
+    }
+    current_element = current_element->NextSiblingElement();
+    if (current_element == NULL) {
+        in_process = false;
+    }
+    return true;
+}
+
+bool Config::get_next_param(string& type, string& f_name, int *args) {
+   if (!in_process) {
+        return false;
+    }
+    TiXmlAttribute *atr = current_element->FirstAttribute();
+    int atr_num = 1;
+    while (atr != NULL) {
+        switch(atr_num) {
+            case 1 : type = *(new string(atr->Value()));
+                     break;
+            case 2 : f_name = *(new string(atr->Value()));
+                     break;
+            default : int pr;
+                      if (atr->QueryIntValue(&pr) == TIXML_SUCCESS) {
+                          args[atr_num - 3] = pr;
+                      }
+                      else {
+                          cout << "Tiny problem!!!" << endl;
+                          return false;
+                      }
+                      break;
+        }
+        atr = atr->Next();
+        atr_num++;
+    }
+    current_element = current_element->NextSiblingElement();
+    if (current_element == NULL) {
+        in_process = false;
+    }
+    return true;
 }
 
 bool Config::get_sign_config(string *config_list) {
     while ( strcmp(current_element->Value(),"sign_config") != 0) {
-    cout << "1. I'm here!!!" << endl;
         current_element = current_element->NextSiblingElement();
         if (current_element == NULL) {
             return false;
@@ -33,7 +105,6 @@ bool Config::get_sign_config(string *config_list) {
             return false;
         }
         config_list[i++] = *(new string(atr->Value()));
-        cout << "Atr " << config_list[i - 1] << endl;
     }
     return true;
 }
--- Configuration.h
+++ Configuration.h
@@ -8,31 +8,35 @@
 class Config {
 private:
     static Config *config;
-protected:
-    char file_name[100];
+    std::string file_name;
     bool in_process;
     TiXmlDocument document;
     TiXmlElement *current_element;
-    Config(const char* f_name);
+protected:
+    Config(std::string f_name);
+    ~Config() { delete config;}
 public:
-    static Config* get_config(const char* f_name) {
-        if (config == 0) {
-            config = new Config(f_name);
-        }
-        return config;
-    }
+    static Config* get_config(std::string f_name) { return new Config(f_name); }
+
+    bool load_xml_file();
 
     bool get_sign_config(std::string *config_list);
 
-    bool is_ready() { return !in_process; }
+    bool get_next_param(std::string& type, std::string& f_name, int *args);
+
+    bool get_next_signature(std::string& signature, std::string& type, int *priority, int *num_pack);
+
+    bool is_ready() const { return !in_process; }
 };
 
 
 class MainConfig : public Config {
 public:
-    MainConfig(const char* f_name) : Config(f_name){
+    MainConfig(std::string f_name) : Config(f_name){
         Config::get_config(f_name);
     }
+
+    ~MainConfig() {};
 };
 
 
